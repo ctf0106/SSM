13de4aad3adae832b5a3ca19e3998093bea60229
ctf0106 <ctf0106@163.com>
2018-07-04 13:20:29 +0800

    freemarker add

---
+++ src/main/java/com/ctf/realm/MyRealm.java
@@ -0,0 +1,40 @@
+package com.ctf.realm;
+
+import javax.annotation.Resource;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+import com.ctf.entity.User;
+import com.ctf.service.UserService;
+
+public class MyRealm extends AuthorizingRealm{
+
+	
+	@Resource
+	private UserService UserService;
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+		return null;
+	}
+	
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+		String userName=(String)token.getPrincipal();
+		User user=UserService.getByUserName(userName);
+		if(user!=null){
+			SecurityUtils.getSubject().getSession().setAttribute("currentUser", user); 
+			AuthenticationInfo authcInfo=new SimpleAuthenticationInfo(user.getUserName(),user.getPassword(),"xx");
+			return authcInfo;
+		}else{
+			return null;				
+		}
+	}
+
+}
